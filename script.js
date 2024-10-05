0a219ef71bfc2b122c4b4fe5e4cb983b431b1888
SriMoorthy <srimoorthy27@gmail.com>
2024-10-06 00:05:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let cnt = 0
+let count = document.getElementById('counter')
+count.innerHTML = cnt;
+let btn = document.getElementById('incrementBtn')
+
+function Increment() {
+	cnt++;
+	count.innerHTML = cnt;
+	alert(cnt);
+}
+
